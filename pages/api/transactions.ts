c04b1e8adb9495a9a8579f2ef88ffb6d55b03356
Emmanuel-uzoezie <mac@macs-MacBook-Pro.local>
2023-09-09 16:15:01 +0100

    implementing solana transfer login

--- pages/api/transactions.ts
+++ pages/api/transactions.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Connection, PublicKey } from '@solana/web3.js';
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { createTransfer } from '../../utiles/createTransfer';
 
 type Data = {
   name: string
@@ -25,5 +27,36 @@ const get = async (req: NextApiRequest,
 };
 
 const post = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("yes is working", res)
+  // Initialize Solana connection
+  const connection = new Connection('https://api.mainnet-beta.solana.com'); // Update the URL if you're using a different network
+
+  // Get sender and recipient public keys from request body
+  const { senderPublicKey: senderKey, recipientPublicKey: recipientKey, amount } = req.body;
+
+  // Validate the keys and amount
+  if (!senderKey || !recipientKey || !amount) {
+    return res.status(400).json({ message: 'Missing required fields' });
+  }
+
+  // Create PublicKey objects
+  const senderPublicKey = new PublicKey(senderKey);
+  const recipientPublicKey = new PublicKey(recipientKey);
+
+  try {
+    const transaction = await createTransfer(
+      connection,
+      senderPublicKey,
+      {
+        recipient: recipientPublicKey,
+        amount: amount,
+      }
+    );
+
+    // TODO: Serialize and sign the transaction, then send it to the Solana network
+
+    res.status(200).json({ message: 'Transaction created', transaction });
+  } catch (error) {
+    console.error('Error creating transaction:', error);
+    res.status(500).json({ message: 'Error creating transaction' });
+  }
 };
