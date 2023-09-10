f173c3c969d8b6a8ee6fab71f3279233ce3a55cf
Emmanuel-uzoezie <mac@macs-MacBook-Pro.local>
2023-09-10 08:50:28 +0100

    added transaction

--- pages/api/transactions.ts
+++ pages/api/transactions.ts
@@ -1,8 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Connection, Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from '@solana/web3.js';
+import { Connection, Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, Commitment, Transaction } from '@solana/web3.js';
+import { createTransferCheckedInstruction, getAccount, getAssociatedTokenAddress, getMint } from '@solana/spl-token';
+// import { LAMPORTS_PER_SOL, SystemProgram, Transaction, TransactionInstruction } from '@solana/web3.js';
 import BigNumber from 'bignumber.js';
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { createTransfer } from '../../utiles/createTransfer';
+import { Amount, Memo, Recipient, References, SPLToken } from '../../utiles/type';
 // import * as base58 from "base-58";
 
 type Data = {
@@ -28,147 +31,131 @@ const get = async (req: NextApiRequest,
   });
 };
 
+export class CreateTransferError extends Error {
+  name = 'CreateTransferError';
+}
+
+/**
+ * Fields of a Solana Pay transfer request URL.
+ */
+export interface CreateTransferFields {
+  /** `recipient` in the [Solana Pay spec](https://github.com/solana-labs/solana-pay/blob/master/SPEC.md#recipient). */
+  recipient: Recipient;
+  /** `amount` in the [Solana Pay spec](https://github.com/solana-labs/solana-pay/blob/master/SPEC.md#amount). */
+  amount: Amount;
+  /** `spl-token` in the [Solana Pay spec](https://github.com/solana-labs/solana-pay/blob/master/SPEC.md#spl-token). */
+  splToken?: SPLToken;
+  /** `reference` in the [Solana Pay spec](https://github.com/solana-labs/solana-pay/blob/master/SPEC.md#reference). */
+  reference?: References;
+  /** `memo` in the [Solana Pay spec](https://github.com/solana-labs/solana-pay/blob/master/SPEC.md#memo). */
+  memo?: Memo;
+}
+
 const post = async (req: NextApiRequest, res: NextApiResponse) => {
-  // Initialize Solana connection
-  // const connection = new Connection('https://api.mainnet-beta.solana.com'); // Update the URL if you're using a different network
-
-  // const senderKey = req.body?.account;
-
-  // // Get sender and recipient public keys and amount from request body
-  // const { receiverPublicKey: recipientKey, amount, userId } = req.query;
-
-  // // Validate the keys and amount
-  // if (!senderKey || !recipientKey || !amount) {
-  //   return res.status(400).json({ message: 'Missing required fields' });
-  // }
-
-  // // Create PublicKey objects
-  // const senderPublicKey = new PublicKey(senderKey);
-  // const recipientPublicKey = new PublicKey(recipientKey);
-
-  // try {
-  //   const transaction = await createTransfer(
-  //     connection,
-  //     senderPublicKey,
-  //     {
-  //       recipient: recipientPublicKey,
-  //       amount: new BigNumber(`${amount}`),
-  //       memo: `${userId}`,
-  //     }
-  //   );
-
-  //   // Assuming you have the sender's Keypair object
-  //   const senderKeypair ="" /* Your Keypair object here */
-
-  //   // Sign the transaction
-  //   transaction.sign(senderKeypair);
-
-  //   // Send the transaction to the Solana network
-  //   const txid = await sendAndConfirmTransaction(connection, transaction, [senderKeypair]);
-
-  //   res.status(200).json({ message: 'Transaction successful', txid });
-  // } catch (error) {
-  //   console.error('Error creating transaction:', error);
-  //   res.status(500).json({ message: 'Error creating transaction' });
-  // }
-
-
-
 
   // const accountField = req.body?.account;
   // if (!accountField) throw new Error('missing account');
 
   // const sender = new PublicKey(accountField);
 
-  // const merchant = Keypair.fromSecretKey(
-  //   new Uint8Array(JSON.parse("[226,230,33,166,183,94,221,240,76,0,177,119,22,166,134,93,69,185,83,121,221,13,229,219,18,55,91,84,86,112,53,87,139,130,97,105,159,216,5,167,211,57,175,154,105,195,156,4,68,100,253,224,35,32,204,44,126,175,226,176,146,254,206,226]")),
-  // );
-
-
-  // // Build Transaction
-  // const ix = SystemProgram.transfer({
-  //   fromPubkey: sender,
-  //   toPubkey: new PublicKey("2eeLQxYpuwpdMxsqCSpYazgxSqdy3wS6DAosATtezHHR"),
-  //   lamports: 133700000
-  // })
-
-  // let transaction = new Transaction();
-  // transaction.add(ix);
-
   // const connection = new Connection("https://api.devnet.solana.com")
-  // const bh = await connection.getLatestBlockhash();
-  // transaction.recentBlockhash = bh.blockhash;
-  // transaction.feePayer = merchant.publicKey;
+  // const senderInfo = await connection.getAccountInfo(sender);
+  // const recipient = new PublicKey("2eeLQxYpuwpdMxsqCSpYazgxSqdy3wS6DAosATtezHHR")
+  // if (!senderInfo) throw new CreateTransferError('sender not found');
 
-  // // for correct account ordering 
-  // transaction = Transaction.from(transaction.serialize({
-  //   verifySignatures: false,
-  //   requireAllSignatures: false,
-  // }));
+  // const recipientInfo = await connection.getAccountInfo(recipient);
+  // if (!recipientInfo) throw new CreateTransferError('recipient not found');
 
-  // transaction.sign(merchant);
-  // // console.log(base58.encode(transaction.signature));
+  // // A native SOL or SPL token transfer instruction
+  // const instruction = splToken
+  //   ? await createSPLTokenInstruction(recipient, amount, splToken, sender, connection)
+  //   : await createSystemInstruction(recipient, amount, sender, connection);
 
-  // // airdrop 1 SOL just for fun
-  // connection.requestAirdrop(sender, 1000000000);
+  // // If reference accounts are provided, add them to the transfer instruction
+  // if (reference) {
+  //   if (!Array.isArray(reference)) {
+  //     reference = [reference];
+  //   }
 
-  // // Serialize and return the unsigned transaction.
-  // const serializedTransaction = transaction.serialize({
-  //   verifySignatures: false,
-  //   requireAllSignatures: false,
-  // });
+  //   for (const pubkey of reference) {
+  //     instruction.keys.push({ pubkey, isWritable: false, isSigner: false });
+  //   }
+  // }
 
-  // const base64Transaction = serializedTransaction.toString('base64');
-  // const message = 'Thank you for using AndyPay';
+  // // Create the transaction
+  // const transaction = new Transaction();
+  // transaction.feePayer = sender;
+  // transaction.recentBlockhash = (await connection.getRecentBlockhash(commitment)).blockhash;
 
-  // // const strategy : TransactionConfirmationStrategy =  {
-  // //   signature: transaction.
-  // // }
-  // // connection.confirmTransaction();
+  // // If a memo is provided, add it to the transaction before adding the transfer instruction
+  // if (memo != null) {
+  //   transaction.add(
+  //     new TransactionInstruction({
+  //       programId: MEMO_PROGRAM_ID,
+  //       keys: [],
+  //       data: Buffer.from(memo, 'utf8'),
+  //     })
+  //   );
+  // }
 
-  // res.status(200).send({ transaction: base64Transaction, message });
+  // // Add the transfer instruction to the transaction
+  // transaction.add(instruction);
+
+  // return transaction;
 
 
-  try {
-    // Initialize Solana connection
-    const connection = new Connection("https://api.devnet.solana.com");
 
-    // Get the sender's public key from the request body
-    const accountField = req.body?.account;
-    if (!accountField) throw new Error('missing account');
-    const sender = new PublicKey(accountField);
 
-    // Securely get the merchant's secret key (Do not hardcode)
-    const merchant = Keypair.fromSecretKey(
+  const accountField = req.body?.account;
+  if (!accountField) throw new Error('missing account');
+
+  const sender = new PublicKey(accountField);
+
+  const merchant = Keypair.fromSecretKey(
     new Uint8Array(JSON.parse("[226,230,33,166,183,94,221,240,76,0,177,119,22,166,134,93,69,185,83,121,221,13,229,219,18,55,91,84,86,112,53,87,139,130,97,105,159,216,5,167,211,57,175,154,105,195,156,4,68,100,253,224,35,32,204,44,126,175,226,176,146,254,206,226]")),
   );
-    // const merchant = Keypair.fromSecretKey(new Uint8Array(merchantSecretKey));
 
-    // Build Transaction
-    const ix = SystemProgram.transfer({
-      fromPubkey: sender,
-      toPubkey: new PublicKey("APaynxjiBJBrEX5rqYBTbmSFN4NhPg6TKzkTmhG7URoX"),
-      lamports: 133700000
-    });
 
-    let transaction = new Transaction();
-    transaction.add(ix);
+  // Build Transaction
+  const ix = SystemProgram.transfer({
+    fromPubkey: sender,
+    toPubkey: new PublicKey("2eeLQxYpuwpdMxsqCSpYazgxSqdy3wS6DAosATtezHHR"),
+    lamports: 133700000
+  })
 
-    // Get the latest blockhash
-    const bh = await connection.getLatestBlockhash();
-    transaction.recentBlockhash = bh.blockhash;
-    transaction.feePayer = merchant.publicKey;
+  let transaction = new Transaction();
+  transaction.add(ix);
 
-    // Sign the transaction
-    transaction.sign(merchant);
+  const connection = new Connection("https://api.devnet.solana.com")
+  const bh = await connection.getLatestBlockhash();
+  transaction.recentBlockhash = bh.blockhash;
+  transaction.feePayer = merchant.publicKey;
 
-    // Send the transaction to the Solana network and confirm it
-    const txid = await sendAndConfirmTransaction(connection, transaction, [merchant]);
+  // for correct account ordering 
+  transaction = Transaction.from(transaction.serialize({
+    verifySignatures: false,
+    requireAllSignatures: false,
+  }));
 
-    // Respond with the transaction ID
-    res.status(200).send({ message: 'Transaction successful', txid });
-  } catch (error) {
-    console.error('Error creating transaction:', error);
-    res.status(500).send({ message: 'Error creating transaction' });
-  }
+  transaction.sign(merchant);
+  // console.log(base58.encode(transaction.signature));
+
+  // airdrop 1 SOL just for fun
+  connection.requestAirdrop(sender, 1000000000);
+
+  // Serialize and return the unsigned transaction.
+  const serializedTransaction = transaction.serialize({
+    verifySignatures: false,
+    requireAllSignatures: false,
+  });
+
+  const base64Transaction = serializedTransaction.toString('base64');
+  const message = 'Thank you for using AndyPay';
+
+  // const strategy : TransactionConfirmationStrategy =  {
+  //   signature: transaction.
+  // }
+  // connection.confirmTransaction();
+
+  res.status(200).send({ transaction: base64Transaction, message });
 };
