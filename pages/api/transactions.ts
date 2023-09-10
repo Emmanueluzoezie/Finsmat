aec7b3261f32988d1f3c86f05531b36d9556b86a
Emmanuel-uzoezie <mac@macs-MacBook-Pro.local>
2023-09-10 14:36:57 +0100

    making sender the fee payer

--- pages/api/transactions.ts
+++ pages/api/transactions.ts
@@ -13,14 +13,6 @@ type Data = {
   name: string
 }
 
-export default function handler(request: NextApiRequest, response: NextApiResponse<Data>
-) {
-  if (request.method === 'GET') return get(request, response);
-  if (request.method === 'POST') return post(request, response);
-
-  throw new Error(`Unexpected method ${request.method}`);
-}
-
 const get = async (req: NextApiRequest,
   res: NextApiResponse) => {
   const label = req.query.product;
@@ -42,9 +34,9 @@ async function post(
 
   const sender = new PublicKey(accountField);
 
-  const merchant = Keypair.fromSecretKey(
-    new Uint8Array(JSON.parse("[226,230,33,166,183,94,221,240,76,0,177,119,22,166,134,93,69,185,83,121,221,13,229,219,18,55,91,84,86,112,53,87,139,130,97,105,159,216,5,167,211,57,175,154,105,195,156,4,68,100,253,224,35,32,204,44,126,175,226,176,146,254,206,226]")),
-  );
+  // const merchant = Keypair.fromSecretKey(
+  //   new Uint8Array(JSON.parse("[226,230,33,166,183,94,221,240,76,0,177,119,22,166,134,93,69,185,83,121,221,13,229,219,18,55,91,84,86,112,53,87,139,130,97,105,159,216,5,167,211,57,175,154,105,195,156,4,68,100,253,224,35,32,204,44,126,175,226,176,146,254,206,226]")),
+  // );
 
 
   // Build Transaction
@@ -60,7 +52,7 @@ async function post(
   const connection = new Connection("https://api.devnet.solana.com")
   const bh = await connection.getLatestBlockhash();
   transaction.recentBlockhash = bh.blockhash;
-  transaction.feePayer = merchant.publicKey;
+  transaction.feePayer = sender;
 
   // airdrop 1 SOL just for fun
   connection.requestAirdrop(sender, 1000000000);
@@ -81,7 +73,7 @@ async function post(
   });
 
   const base64Transaction = serializedTransaction.toString('base64');
-  const message = 'Thank you for using AndyPay';
+  const message = 'Thank you for using Jonuel';
 
   // const strategy : TransactionConfirmationStrategy =  {
   //   signature: transaction.
@@ -92,4 +84,12 @@ async function post(
 
 }
 
+
+export default function handler(request: NextApiRequest, response: NextApiResponse<Data>
+) {
+  if (request.method === 'GET') return get(request, response);
+  if (request.method === 'POST') return post(request, response);
+
+  throw new Error(`Unexpected method ${request.method}`);
+}
 
