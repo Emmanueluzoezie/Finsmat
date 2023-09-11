683f5112ca8ca5ea54947f23ab4a6519d0bd4976
Emmanuel-uzoezie <mac@macs-MacBook-Pro.local>
2023-09-11 02:37:13 +0100

    hopefully the transaction will be successful

--- pages/api/transactions.ts
+++ pages/api/transactions.ts
@@ -7,7 +7,7 @@ type GetData = {
   icon: string
 }
 type PostData = {
-  transaction: string,
+  transaction?: string,
   message?: string
 }
 
@@ -15,7 +15,7 @@ function get(
   req: NextApiRequest,
   res: NextApiResponse<GetData>
 ) {
-  const label = 'SolAndy Pay';
+  const label = 'Payment your item';
   const icon = 'https://avatars.githubusercontent.com/u/92437260?v=4';
 
   res.status(200).send({
@@ -28,7 +28,6 @@ async function post(
   req: NextApiRequest,
   res: NextApiResponse<PostData>
 ) {
-  // Account provided in the transaction request body by the wallet.
   const accountField = req.body?.account;
   if (!accountField) throw new Error('missing account');
 
@@ -38,35 +37,39 @@ async function post(
     new Uint8Array(JSON.parse("[226,230,33,166,183,94,221,240,76,0,177,119,22,166,134,93,69,185,83,121,221,13,229,219,18,55,91,84,86,112,53,87,139,130,97,105,159,216,5,167,211,57,175,154,105,195,156,4,68,100,253,224,35,32,204,44,126,175,226,176,146,254,206,226]")),
   );
 
-
-  // Build Transaction
   const ix = SystemProgram.transfer({
     fromPubkey: sender,
     toPubkey: new PublicKey("2eeLQxYpuwpdMxsqCSpYazgxSqdy3wS6DAosATtezHHR"),
     lamports: 133700000
-  })
+  });
 
   let transaction = new Transaction();
   transaction.add(ix);
 
-  const connection = new Connection("https://api.devnet.solana.com")
+  const connection = new Connection("https://api.devnet.solana.com");
   const bh = await connection.getLatestBlockhash();
   transaction.recentBlockhash = bh.blockhash;
   transaction.feePayer = merchant.publicKey;
 
-  // for correct account ordering 
   transaction = Transaction.from(transaction.serialize({
     verifySignatures: false,
     requireAllSignatures: false,
   }));
 
   transaction.sign(merchant);
-  // console.log(base58.encode(transaction.signature));
 
-  // airdrop 1 SOL just for fun
-  connection.requestAirdrop(sender, 1000000000);
+  // Simulate the transaction
+  const simulationResult = await connection.simulateTransaction(transaction);
 
-  // Serialize and return the unsigned transaction.
+  if (simulationResult.value.err) {
+    console.error(`Error with transaction: ${simulationResult.value.err}`);
+    res.status(400).send({ message: `Transaction simulation failed: ${simulationResult.value.err}` });
+    return;
+  }
+
+  // Airdrop 1 SOL
+  await connection.requestAirdrop(sender, 1000000000);
+
   const serializedTransaction = transaction.serialize({
     verifySignatures: false,
     requireAllSignatures: false,
@@ -75,24 +78,5 @@ async function post(
   const base64Transaction = serializedTransaction.toString('base64');
   const message = 'Thank you for using AndyPay';
 
-  // const strategy : TransactionConfirmationStrategy =  {
-  //   signature: transaction.
-  // }
-  // connection.confirmTransaction();
-
   res.status(200).send({ transaction: base64Transaction, message });
-
-}
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<GetData | PostData>
-) {
-  if (req.method == "GET") {
-    console.log("received GET request");
-    return get(req, res);
-  } else if (req.method == "POST") {
-    console.log("received POST request");
-    return await post(req, res);
-  }
 }
